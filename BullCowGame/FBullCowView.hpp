a9aba260772d79568e9cd4dada38927b6b83a85b
Diego Becciolini <diego.becciolini@gmail.com>
2017-01-19 09:46:35 +0000

    Pesky markup.

--- BullCowGame/FBullCowView.hpp
+++ BullCowGame/FBullCowView.hpp
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Itizir.
 //  This work is free. You can redistribute it and/or modify it under the
 //  terms of the Do What The Fuck You Want To Public License, Version 2,
-//  as published by Sam Hocevar. See the LICENSE.md file for more details.
+//  as published by Sam Hocevar. See the LICENSE file for more details.
 //
 
 
